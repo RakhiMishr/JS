72ecf79fc67f09b062c886a5e7ab4fdc1e0f248c
RakhiMishr <rakhimishra9572@gmail.com>
2025-04-15 21:31:55 +0530

    Add files via upload

---
+++ Date/dete.js
@@ -0,0 +1,21 @@
+// let date=new Date();
+// console.log(date);
+
+let date=new Date();
+ console.log(date.getDate());
+ console.log(date.getMonth()+1);
+ console.log(date.getFullYear());
+ console.log(date.getHours());
+ console.log(date.getMinutes());
+ console.log(date.getSeconds());
+ console.log(date.getMilliseconds());
+ console.log(date.getTime());
+ console.log(date.getDay());
+ console.log(date.getTimezoneOffset());
+ console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleString());
+console.log(date.toLocaleDateString());
+
+
