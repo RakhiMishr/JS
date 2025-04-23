289735dfb5d188f02d7ba65defd40815d494481b
RakhiMishr <rakhimishra9572@gmail.com>
2025-04-23 21:48:23 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,18 @@
+/* boolean date type*/
+var a = true;
+var b = false;
+
+
+
+
+console.log("a && b = " + (a && b)); 
+/*console.log("a || b = " + (a || b) );
+console.log("!a = " + (!a) );
+console.log("!b = " + (!b) );*/
+
+/*var a = 10;
+var b = '20';
+var c = "30";
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);*/
