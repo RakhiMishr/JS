919a10a476943356056da50cb00ad7f967a6189b
RakhiMishr <rakhimishra9572@gmail.com>
2025-04-23 21:47:48 +0530

    Add files via upload

---
+++ controlStatements.js
@@ -0,0 +1,15 @@
+//let name=alert("enter your name");
+//let name=prompt("enter your name");
+//document.writeIn(name);
+
+
+let age=prompt("enter your name");
+if(age > 21 && age < 35){
+document.writeln("ap kr skte ho jee");
+}
+else if(age <= 21 ){
+    document.writeln("ap wait kr skte");
+}
+else{
+    document.writeln("ap nhi kr skte");
+}
